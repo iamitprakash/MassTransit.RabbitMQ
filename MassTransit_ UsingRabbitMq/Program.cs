65331645099f6d701269eebef8848542a6315e67
Amit Prakash <34869115+iamitprakash@users.noreply.github.com>
2024-02-25 02:39:35 +0530

    Add files via upload
    Initial commit

---
+++ MassTransit_ UsingRabbitMq/Program.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(busConfigurator =>
+{
+    busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+    busConfigurator.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host("localhost", "/", h =>
+        {
+            h.Username("admin");
+            h.Password("admin");
+        });
+
+        configurator.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
